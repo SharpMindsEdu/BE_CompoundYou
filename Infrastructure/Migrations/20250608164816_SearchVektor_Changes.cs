f23acf2339d74b1dadffef6a2ff824598a089b15
Fabian Reichenberg <fabian.reichenberg@aboat-entertainment.com>
2025-06-08 18:48:35 +0200

    - Update Migrations

---
+++ Infrastructure/Migrations/20250608164816_SearchVektor_Changes.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using NpgsqlTypes;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SearchVektor_Changes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<NpgsqlTsVector>(
+                name: "title_search_vector",
+                schema: "public",
+                table: "habit",
+                type: "tsvector",
+                nullable: false,
+                oldClrType: typeof(NpgsqlTsVector),
+                oldType: "tsvector")
+                .Annotation("Npgsql:TsVectorConfig", "English")
+                .Annotation("Npgsql:TsVectorProperties", new[] { "title" })
+                .OldAnnotation("Npgsql:TsVectorConfig", "german")
+                .OldAnnotation("Npgsql:TsVectorProperties", new[] { "title" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<NpgsqlTsVector>(
+                name: "title_search_vector",
+                schema: "public",
+                table: "habit",
+                type: "tsvector",
+                nullable: false,
+                oldClrType: typeof(NpgsqlTsVector),
+                oldType: "tsvector")
+                .Annotation("Npgsql:TsVectorConfig", "german")
+                .Annotation("Npgsql:TsVectorProperties", new[] { "title" })
+                .OldAnnotation("Npgsql:TsVectorConfig", "English")
+                .OldAnnotation("Npgsql:TsVectorProperties", new[] { "title" });
+        }
+    }
+}
