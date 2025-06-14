bb58d8b9e13d1242c0ea6a35e47c9477f9ad6591
Fabian Reichenberg <fabian.reichenberg@aboat-entertainment.com>
2025-06-14 17:24:09 +0200

    - Add Migrations

---
+++ Infrastructure/Migrations/20250610122504_AddLoginSecrets.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLoginSecrets : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "sign_in_secret",
+                schema: "public",
+                table: "user",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "sign_in_tries",
+                schema: "public",
+                table: "user",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "sign_in_secret",
+                schema: "public",
+                table: "user");
+
+            migrationBuilder.DropColumn(
+                name: "sign_in_tries",
+                schema: "public",
+                table: "user");
+        }
+    }
+}
